980d047b26ddd5f08ef157354e95d871919097c8
ruh078 <ruhijoshi807@gmail.com>
2021-11-17 10:19:14 +0530

    first commit

---
+++ home_fix/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from home_fix.models import Service_provider, Service_requested, Contact_Us, User_type
+
+# Register your models here.
+admin.site.register(Service_provider)
+admin.site.register(Service_requested)
+admin.site.register(Contact_Us)
+admin.site.register(User_type)
+
